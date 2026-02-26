0db9fce0b45ed3f398859edc558060a3e50ed328
0xManusdev <evame.tofa@gmail.com>
2026-02-26 16:43:58 +0000

    Add error handling and change directory in start script

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 : "${PORT:=8000}"
 # Use gunicorn + uvicorn worker for production
+set -e
+cd /app
 exec gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT --workers 2
